6988dc7ad0cca816cf8da7eddd00b9a24a65d2f8
Martin Grahovski <martingrahovski2004@gmail.com>
2022-09-25 15:43:30 +0300

    a

---
+++ BlazorApp/BlazorApp/Data/Grocery.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Data
+{
+    public class Grocery
+    {
+        [Required]
+        [StringLength(20, ErrorMessage="Name should be less than 20 characters.")]
+        public string Name { get; set; }
+        [Required]
+        [Range(0.1,100000,ErrorMessage ="Between 0.1 and 100000")]
+        public decimal Price { get; set; }
+    }
+}
